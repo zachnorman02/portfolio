f2fcee8061212911407764d3059c7f1ac40c764f
Zach Norman <zachnorman02@gmail.com>
2023-03-18 22:00:28 -0400

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import "../styles/footer.css";
+
+function Footer () {
+    return (
+        <div className="footer">
+            <p>...</p>
+            <p>Zach Norman | {new Date().getFullYear()}</p>
+        </div>
+    )
+}
+
+export default Footer;
