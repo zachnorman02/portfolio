801004f99cde3dbf49e0e5a9ed43de777ba16cb8
Zach Norman <zachnorman02@gmail.com>
2024-10-10 02:40:29 -0400

    update resume code

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,16 +5,31 @@ import Experience from "./ResumePieces/Experience";
 import School from "./ResumePieces/School";
 import Skills from "./ResumePieces/Skills";
 import ResumePDF from "../Norman_Zach_Resume.pdf"
+import preval from "babel-plugin-preval/macro";
+
+const lastModified = preval`
+  const fs = require('fs');
+  const path = require('path');
+  const filePath = path.join(__dirname, '../Norman_Zach_Resume.pdf');
+  const stats = fs.statSync(filePath);
+  module.exports = stats.mtime.toISOString();
+`;
+
+const formattedDate = new Date(lastModified).toLocaleString("en-US", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+});
 
 function Resume() {
   return (
     <div className="content">
       <h1>Resume</h1>
-      <a
-        href={ResumePDF}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href={ResumePDF} target="_blank" rel="noopener noreferrer">
         PDF Version
       </a>{" "}
       <a
@@ -24,7 +39,7 @@ function Resume() {
       >
         LinkedIn
       </a>
-      <p>Last Updated: Nov 2023</p>
+      <p>PDF Last Updated: {formattedDate} (updates automatically)</p>
       <Skills />
       <Certs />
       <div className="timelines">
